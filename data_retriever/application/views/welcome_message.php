fe71649ad3eb27ef5c07adb87c5fa767d3b00f0a
john gills <john.gills@gmail.com>
2012-10-18 19:10:09 -0400

    adding twitter scoller

--- data_retriever/application/views/welcome_message.php
+++ data_retriever/application/views/welcome_message.php
@@ -4,7 +4,7 @@
   <div class="span12">
   	<div class="tops">
   		<img src="/img/logo_a.png" />
-    	<h2>header</h2>
+    	<h2>The Race</h2>
     </div>
     <div class="row-fluid">
 
@@ -45,8 +45,13 @@
    	    </div>
       
       </div>
-      <div class="span6 tweetBox">Level 2
-      		<i class="icon-search"></i>
+      <div class="span6 tweetBox">
+      	<h2>Recent Conversation</h2>
+  		<div id="feeds">
+            <div class="item" id="feed0" style="display: none;">YOURCONTENTS</div>
+            <div class="item" id="feed1" style="display: none;">YOURCONTENTS</div>
+            <div class="item" id="feed2" style="display: none;">YOURCONTENTS</div>
+		</div>
       </div>
     </div>
   </div>
@@ -55,56 +60,78 @@
 
 <script type="text/javascript">
 
-    $(document).ready(function() {
-    
-    // assume we can use 80% of the div we are in and set the leader to 100% of that 80%
-    // divide the losers to their percentage of the winners score in the 80%
-    var wait = 1000,
-    	highScore = 24,
-    	racers = [
-    			{
-    				name:	'cti_1',
-    				score:	12,
-    				pos:	5,
-    				neg:	1,
-    				ntl:	2
-    			},
-    			{
-    				name:	'cti_2',
-    				score:	24,
-    				pos:	33,
-    				neg:	4,
-    				ntl:	5
-    			}	
-    				
-    		],
-    	rDiv,multiplier,faceMargin,barWidth,score;
-	
-    for (var i = 0; i < racers.length; i++) {
-		rDiv 	  = 'div#' + racers[i].name;
-		score	  = parseInt(racers[i].score,10);
-		multiplier = 60 / highScore;	
-		faceMargin = score * multiplier;
-		barWidth   = faceMargin + 10;
-    
-    	$(rDiv).find('.avatar').animate({    
-    	    'margin-left': faceMargin + '%'
-    	  }, 2000, function() {
-    	    // Animation complete.
-    	  }).delay(wait);
-    	$(rDiv).find('.bar').animate({    
-    	    'width': barWidth + '%'
-    	  }, 2000, function() {
-    	    // Animation complete.
-    	  }).delay(wait);  
-    	$(rDiv).find('.avatar').click(function() {
-  	      if ($(rDiv).find('.stats').is(":hidden")) {
-  	    		$(rDiv).find('.stats').slideDown("slow");
-  	        } else {
-  	        	$(rDiv).find('.stats').slideUp("slow");
-  	        }
-  		});
-    }
+    var highScore = 24,
+    	delay = 2000, // you can change it
+    	count = 5, // How much items to animate
+    	showing = 3, //How much items to show at a time
+    	i = 0,
+        racers = [
+        		{
+        			name:	'cti_1',
+        			score:	12,
+        			pos:	5,
+        			neg:	1,
+        			ntl:	2
+        		},
+        		{
+        			name:	'cti_2',
+        			score:	24,
+        			pos:	33,
+        			neg:	4,
+        			ntl:	5
+        		}	
+        			
+        	];
 
+	function animateTerms() {
+		var r = racers,
+			h = highScore,
+			rDiv,multiplier,faceMargin,barWidth,score;
+	
+        for (var i = 0; i < r.length; i++) {
+    		rDiv 	  = 'div#' + r[i].name;
+    		score	  = parseInt(r[i].score,10);
+    		multiplier = 60 / h;	
+    		faceMargin = score * multiplier;
+    		barWidth   = faceMargin + 10;
+        
+        	$(rDiv).find('.avatar').animate({    
+        	    'margin-left': faceMargin + '%'
+        	  }, 2000, function() {
+        	    // Animation complete.
+        	  });
+        	$(rDiv).find('.bar').animate({    
+        	    'width': barWidth + '%'
+        	  }, 2000, function() {
+        	    // Animation complete.
+        	  });  
+        	$(rDiv).find('.avatar').click(function() {
+      	      if ($(rDiv).find('.stats').is(":hidden")) {
+      	    		$(rDiv).find('.stats').slideDown("slow");
+      	        } else {
+      	        	$(rDiv).find('.stats').slideUp("slow");
+      	        }
+      		});
+        }
+	}
+
+    function move(i) {
+        return function() {
+          $('#feed'+i).remove().css('display', 'none').prependTo('#feeds');
+        }
+      }
+
+    function shift() {
+        var toShow = (i + showing) % count;
+        $('#feed'+toShow).slideDown(1000, move(i));
+        $('#feed'+i).slideUp(1000, move(i));
+        i = (i + 1) % count;
+        setTimeout('shift()', delay);
+    }  	
+
+    $(document).ready(function() {
+    	setTimeout('shift()', delay);
+    	setTimeout('animateTerms()', delay);
     });
+
 </script>
